3398ba8e7e6532617ade15fb9a56a24e178de228
AI_Nomads <chugue85@gmail.com>
2024-02-06 20:42:49 +0900

    logout implemented

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,13 +1,31 @@
 package shop.mtcoding.blog.board;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class BoardController {
+    private final BoardRepository boardRepository;
+    private final HttpSession session;
+
+    @GetMapping("/logout")
+    public String logout() {
+        session.invalidate();
+        return "redirect:/";
+    }
 
     @GetMapping({ "/", "/board" })
-    public String index() {
+    public String index(HttpServletRequest request) {
+        List<Board> boardList = boardRepository.findAll();
+
+        request.setAttribute("boardList", boardList);
         return "index";
     }
 
@@ -16,8 +34,17 @@ public class BoardController {
         return "board/saveForm";
     }
 
-    @GetMapping("/board/1")
-    public String detail() {
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable int id, HttpServletRequest request) {
+
+        // 모델에게 위임하고 반환된 값을 responseDTO에 저장
+        BoardResponse.DetailDTO responseDTO = boardRepository.findById(id);
+
+        // request가방에 담아서 화면에 렌더링할 수 있게해준다.
+        // 아래 코드 예시로는 mustache에서 {{board}} 같이 문법을 쓰면 정보를 가져다 쓸 수 있다.
+        request.setAttribute("board", responseDTO);
         return "board/detail";
     }
+
+
 }
---
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -0,0 +1,54 @@
+package shop.mtcoding.blog.board;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BoardRepository {
+    private final EntityManager em;
+
+    public List<Board> findAll() {
+        Query query = em.createNativeQuery("select * from board_tb order by id desc", Board.class);
+        return query.getResultList();
+    }
+
+    public BoardResponse.DetailDTO findById(int idx) {
+        // 이 코드를 사용하려면 의존성주입이 되어야 한다. EntityManager
+        Query query = em.createNativeQuery("select b.id, b.title, b.content, b.user_id, u.username from board_tb b inner join user_tb u on b.user_id = u.id where b.id = ?");
+        query.setParameter(1, idx);
+
+        // 객체 각 요소들을 배열로 받는다. 한 행의 요소로 배열로 들어간다.
+        Object[] row = (Object[]) query.getSingleResult();
+
+        // H2에 받은 결과값을 그대로 확인하고 타입과 순서를 모두 지켜줘야 된다. 1차원적 설계
+        Integer id = (Integer) row[0];
+        String title = (String) row[1];
+        String content = (String) row[2];
+        int userId = (Integer) row[3];
+        String username = (String) row[4];
+
+        //확인용 코드
+        System.out.println("id : "+id);
+        System.out.println("title : "+title);
+        System.out.println("content : "+content);
+        System.out.println("userId : "+userId);
+        System.out.println("username : "+username);
+
+        //객체를 생성하여서 DetailDTO로 옮겨담는 작업
+        BoardResponse.DetailDTO responseDTO = new BoardResponse.DetailDTO();
+        responseDTO.setId(id);
+        responseDTO.setTitle(title);
+        responseDTO.setContent(content);
+        responseDTO.setUserId(userId);
+        responseDTO.setUsername(username);
+
+        return responseDTO;
+    }
+
+}
